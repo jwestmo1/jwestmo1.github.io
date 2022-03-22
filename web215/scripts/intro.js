e2a7dc4d60009924ba4744b15dae11525e69a9db
Jen Westmoreland <jwestmo1@email.cpcc.edu>
2022-03-22 10:17:41 -0400

    Update intro.js

--- web215/scripts/intro.js
+++ web215/scripts/intro.js
@@ -22,7 +22,7 @@ function getData() {
   let markedCheckbox = document.querySelectorAll('input[name="choices"]:checked');;
   let output = [];
   markedCheckbox.forEach((checkbox) => {
-    output.push.wordSpacing(checkbox.value)
+    output.push(checkbox.value)
 
   });
   var finalChecks = ("You are interested in: " + output + " ");
