9f1e829d83cbc013af8e6aac89ced9e24653a9d3
Jen Westmoreland <jwestmo1@email.cpcc.edu>
2022-04-06 18:38:14 -0400

    Update footer.php

--- web250_PHP/contents/samsusedcars/footer.php
+++ web250_PHP/contents/samsusedcars/footer.php
@@ -1,4 +1,4 @@
 <?php
- echo "<br><br><a href='samsusedcars.html'>Sam's Used Cars Home</a>";
+ echo "<br><br><a href='/samsusedcars/samsusedcars.html'>Sam's Used Cars Home</a>";
   echo "<br>Copyright 2017, Alan Forbes.  All rights reserved.";
 ?>
